e5fbaad283a3e3e5b6ec75ac7f4b4070879339a1
Kalman Varga <kalamane007@gmail.com>
2022-08-17 11:16:36 +0200

    ....

--- generate-ocsg.php
+++ generate-ocsg.php
@@ -1,5 +1,5 @@
 <?php
-ini_set("display_errors", 1);
+ini_set("display_errors", 0);
 
 require_once("TCPDF/tcpdf.php");
 require_once("models/dataSzivessegi.php");
@@ -546,41 +546,53 @@ EOD;
         </p>
         <p style="line-height: 16px;"><b>IV/4.</b> Rendelkezés a nyereség és veszteség tagok közötti megosztásáról
         </p>
-        <p style="line-height: 16px;">A tagok a közös tevékenység nyereségét és veszteségét egymás között az alábbi arányban osztják fel:
-        </p>
 EOD;
 
         $reszesedesek = "";
-        
-        foreach($data->ocsTags as $ocsTag)
+
+        if($data->nyeresegVesztesegMegosztasa == 2)
         {
-          $reszesedesek .= <<<EOD
-          <tr>
-            <td style="width: 5%;">
-              &nbsp;
-            </td>
-            <td style="width: 25%;">Tag neve:
-            </td>
-            <td>$ocsTag->fullName
-            </td>
-            <td style="width: 35%;">Részesedése:
-            </td>
-            <td>$ocsTag->reszesedese%
-            </td>
-          </tr>
-          <tr>
-            <td style="line-height: 4px;">
-              &nbsp;
-            </td>
-          </tr>
+          $html .= <<<EOD
+          <p style="line-height: 16px;">A tagok a közös tevékenység nyereségét és veszteségét egymás között az alábbi arányban osztják fel:
+          </p>
+EOD;
+          
+          foreach($data->ocsTags as $ocsTag)
+          {
+            $reszesedesek .= <<<EOD
+            <tr>
+              <td style="width: 5%;">
+                &nbsp;
+              </td>
+              <td style="width: 25%;">Tag neve:
+              </td>
+              <td>$ocsTag->fullName
+              </td>
+              <td style="width: 35%;">Részesedése:
+              </td>
+              <td>$ocsTag->reszesedese%
+              </td>
+            </tr>
+            <tr>
+              <td style="line-height: 4px;">
+                &nbsp;
+              </td>
+            </tr>
+EOD;
+          }
+          $html .= <<<EOD
+          <table cellspacing="0" cellpadding="0" border="0" style="line-height: 15px;">
+            $reszesedesek
+          </table>
 EOD;
         }
-
-        $html .= <<<EOD
-        <table cellspacing="0" cellpadding="0" border="0" style="line-height: 15px;">
-            $reszesedesek
-        </table>
+        else
+        {
+          $html .= <<<EOD
+            <p style="line-height: 16px;">A közös tevékenység nyeresége és vesztesége egymás között egyenlő arányban oszlik meg.
+            </p>
 EOD;
+        }
 
         $html .= <<<EOD
         <p style="line-height: 16px;"><b>IV/5.</b> Az őstermelők családi gazdaságának tagjai rögzítik, hogy ha valamely tag harmadik személlyel kötött szerződés alapján a IV/4. pontban rögzített veszteség viselésének arányától eltérően köteles helytállni, úgy a tagok kötelesek a IV/4. pontban meghatározottak szerint egymással elszámolni.
@@ -606,6 +618,214 @@ EOD;
 EOD;
         }
 
+        $html .= <<<EOD
+        <p style="line-height: 16px;"><b>V/2.</b> Az őstermelők családi gazdaságának tagjai rögzítik, hogy az V/1. pontban kijelölt őstermelők családi gazdaságának képviselője a többi tag képviseletében az őstermelők családi gazdaságának tevékenysége körében jogokat szerezhet és kötelezettségeket vállalhat.
+        </p>
+        <p style="line-height: 16px;"><b>V/3.</b> Az őstermelők családi gazdaságának tagjai tudomásul veszik, hogy az V/1. pontban kijelölt, őstermelők családi gazdaságának képviselője jogosult az őstermelők családi gazdaságának az őstermelő nyilvántartásba vétele iránti kérelmet előterjeszteni.
+        </p>
+        <p style="line-height: 16px;"><b>V/4.</b> Az őstermelők családi gazdaságának tagjai tudomásul veszik, hogy az V/1. pontban kijelölt őstermelők családi gazdaságának képviselője az őstermelők családi gazdaságának képviseletére az őstermelők családi gazdaságának a nyilvántartásba vételével válik jogosulttá.
+        </p>
+        <p>
+          &nbsp;
+        </p>
+        <h2 style="line-height: 16px;">VI. AZ ŐSTERMELŐK CSALÁDI GAZDASÁGÁNAK KÖZPONTJA
+        </h2>
+        <p style="line-height: 1px;">
+          &nbsp;
+        </p>
+        <p style="line-height: 16px;"><b>VI/1.</b> Az őstermelők családi gazdaságának tagjai rögzítik, hogy az őstermelők családi gazdaságának <b>központja címe:</b> $data->kpPostalCode $data->kpSettlement, $data->kpKozterNev $data->kpKozterTipus $data->kpHazSzam $data->kpEmeletAjto ingatlan.
+        </p>
+        <p>
+          &nbsp;
+        </p>
+        <h2 style="line-height: 16px;">VII. RENDELKEZÉS A SZERZŐDÉS MÓDOSÍTÁSÁRÓL, MEGSZÜNTETÉSÉRŐL
+        </h2>
+        <p style="line-height: 16px;"><b>VII/1.</b> Jelen szerződést a családi gazdaság tagjai jogosultak bármikor közös megegyezéssel írásban módosítani, vagy megszüntetni.
+        </p>
+EOD;
+
+        if($data->hasznalatIdotartamHatarozott == 1)
+        {
+          $html .= <<<EOD
+          <p style="line-height: 16px;"><b>VII/2.</b> Határozott időre kötött szerződés esetén a tagot a rendes felmondás joga nem illeti meg. 
+          </p>
+EOD;
+        }
+        else
+        {
+          $html .= <<<EOD
+          <p style="line-height: 16px;"><b>VII/2.</b> Határozatlan időre kötött szerződés esetén: A jelen szerződést bármely tag 3 hónapos felmondási idővel a többi taghoz intézett írásbeli nyilatkozatával felmondhatja. Tagok rögzítik, hogy azon tag, aki alkalmatlan időben gyakorolja felmondási jogát, köteles az ebből eredő károkat megtéríteni, feltéve, hogy az időpont alkalmatlanságára a többi tag figyelmeztette, és felmondását a figyelmeztetés ellenére fenntartotta. 
+          </p>
+EOD;
+        }
+        $html .= <<<EOD
+        <p style="line-height: 16px;"><b>VII/3.</b> Az őstermelők családi gazdaságának tagjai tudomásul veszik, hogy jelen szerződés megszűnése után a tagok között elszámolásnak van helye. Az elszámolás során a közös vagyont tagok között jelen szerződés III. pontjában rögzített hozzájárulásuk arányában kell felosztani. A közös használatba adott dolgokat vissza kell szolgáltatni annak a tagnak, aki azt az őstermelők családi gazdasága használatába adta, rendelkezésére bocsájtotta.
+        </p>
+EOD;
+
+        $nextNum = "VIII";
+        $pontokSzama = "8";
+        if($data->tovabbvitelModja)
+        {
+          $nextNum = "IX";
+          $pontokSzama = "9";
+
+          $html .= <<<EOD
+          <p>
+            &nbsp;
+          </p>
+          <h2 style="line-height: 16px;">VIII. AZ ŐSTERMELŐK CSALÁDI GAZDASÁGÁNAK TAGJAI AZ ŐSTERMELŐK CSALÁDI GAZDASÁGÁNAK NYILVÁNTARTÁSBA VÉTELÉT MEGELŐZŐEN GAZDÁLKODÁSUK FOLYTATÁSA ÉRDEKÉBEN VÁLLALT KÖTELEZETTSÉGEK ÉS MEGSZERZETT JOGOK ŐSTERMELŐK CSALÁDI GAZDASÁGÁNAK KERETEIN BELÜL TÖRTÉNŐ TOVÁBBVITELÉNEK MÓDJA
+          </h2>
+          <p style="line-height: 16px;">$data->tovabbvitelModja
+          </p>
+EOD;
+        }
+
+        $date = getdate();
+        $datum = $date["year"].". év ". $date["mon"].". hó ".$date["mday"].". nap.";
+
+        $html .= <<<EOD
+        <p>
+          &nbsp;
+        </p>
+        <h2 style="line-height: 16px;">$nextNum. EGYÉB RENDELKEZÉSEK
+        </h2>
+        <p style="line-height: 1px;">
+          &nbsp;
+        </p>
+        <p style="line-height: 16px;"><b>$nextNum/1.</b> A jelen szerződésben nem szabályozott kérdésekben a Törvény, valamint a Polgári Törvénykönyvről szóló 2013. évi V. törvény polgári jogi társasági szerződésre vonatkozó rendelkezéseit kell alkalmazni.
+        </p>
+        <p style="line-height: 16px;"><b>$nextNum/2.</b> Jelen szerződés az aláírás napján lép hatályba.
+        </p>
+        <p style="line-height: 16px;"><b>$nextNum/3.</b> Jelen szerződés - amely ..... oldalból és $pontokSzama fő pontból áll, továbbá 3 egymással mindenben megegyező példányban készült - elolvasás és kölcsönös értelmezés után, a tagok, mint akaratukkal mindenben megegyezőt írták alá.
+        </p>
+        <p>
+          &nbsp;
+        </p>
+        <p style="line-height: 16px;">$datum
+        </p>
+EOD;
+
+        $alairashelyek = "";
+        $i = 0;
+        $j = 1;
+        foreach($data->ocsTags as $ocsTag)
+        {
+          if($i % 2 == 0)
+          {
+            $alairashelyek .= "<tr>";
+          }
+
+          $alairashelyek .= <<<EOD
+          <td style="width: 50%; text-align: center;">
+            _______________________________________
+            <br>
+            $j. számú családtag
+          </td>
+EOD;
+
+          if($i % 2 == 1 || ($i+1) == count($data->ocsTags))
+          {
+            $alairashelyek .= "</tr><tr><td>&nbsp;</td></tr><tr><td>&nbsp;</td></tr>";
+          }
+
+          $i++;
+          $j++;
+        }
+
+        $html .= <<<EOD
+        <p style="line-height: 30px;">
+            &nbsp;
+        </p>
+        <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+            $alairashelyek
+        </table>
+        <p style="line-height: 15px;">
+        &nbsp;
+    </p>
+    <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+        <tr>
+            <td style="width: 50%; text-align: center;">
+                Előttünk, mint tanúk előtt:
+            </td>
+            <td style="width: 50%; text-align: center;">
+                &nbsp;
+            </td>
+        </tr>
+    </table>
+
+    <p style="line-height: 15px;">
+        &nbsp;
+    </p>
+    <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+        <tr>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                aláírás
+            </td>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                aláírás
+            </td>
+        </tr>
+    </table>
+
+    <p style="line-height: 15px;">
+        &nbsp;
+    </p>
+    <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+        <tr>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                név
+            </td>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                név
+            </td>
+        </tr>
+    </table>
+
+    <p style="line-height: 15px;">
+        &nbsp;
+    </p>
+    <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+        <tr>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                lakcím
+            </td>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                lakcím
+            </td>
+        </tr>
+    </table>
+
+    <p style="line-height: 15px;">
+        &nbsp;
+    </p>
+    <table cellspacing="0" cellpadding="0" border="0" style="line-height: 20px;">
+        <tr>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                személyazonosító okmány száma
+            </td>
+            <td style="width: 50%; text-align: center;">
+                _______________________________________
+                <br>
+                személyazonosító okmány száma
+            </td>
+        </tr>
+    </table>
+EOD;
 
         $pdf->writeHTML($html, true, false, false, false);
         $filename = "ocsg_".date("yyyy_m_d_".str_replace(array(' ', '.'), '', (string)microtime())).".pdf";
